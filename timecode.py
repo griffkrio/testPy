42fc219974458339e1f78011f1c9dd07148defd6
kriogen <negoirk@gmail.com>
2019-11-07 10:21:08 +0200

    newfile test

---
+++ timecode.py
@@ -0,0 +1,22 @@
+while True:
+    import os.path, time
+    (mode, ino, dev, nlink, uid, gid, size, atime, mtime, ctime) = os.stat('/home/creativetrendadmin/pythonTEst/TestBuild1.txt')
+    a=mtime
+    b=ctime
+    import os.path, time
+    (mode, ino, dev, nlink, uid, gid, size, atime, mtime, ctime) = os.stat('/home/creativetrendadmin/pythonTEst/TestBuild1_Repotester_Build2Test.buildNumbers.properties')
+    c=ctime
+    d=mtime
+    while a!=d:
+            if a!=d:
+                if a > d :
+                    import shutil
+                    shutil.copy('/home/creativetrendadmin/pythonTEst/TestBuild1.txt', '/home/creativetrendadmin/pythonTEst/TestBuild1_Repotester_Build2Test.buildNumbers.properties')
+                    break
+                    print(d==a)
+                elif d > a:
+                    import shutil
+                    shutil.copy('/home/creativetrendadmin/pythonTEst/TestBuild1_Repotester_Build2Test.buildNumbers.properties','/home/creativetrendadmin/pythonTEst/TestBuild1.txt')
+                    break
+                    print(d==a)
+    print('ok')
